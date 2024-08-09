063fdb905db6b7aaf3339797b9d8ac50017f7848
Taks07 <yutakaoishi7@gmail.com>
2024-08-10 00:35:43 +1200

    Added basic code to handle logins. Token is simply printed

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -7,6 +7,18 @@ export default function LoginForm() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
+    function handleSubmit(e) {
+        e.preventDefault();
+        axios.post("http://localhost:4000/user/login", {
+            email: email,
+            password: password
+        }).then(response => {
+            console.log(response.data);
+        }).catch(error => {
+            console.log(error);
+        });
+    }
+
     return (
         <form className="flex flex-col border-2 items-center border-blue-800 w-80 p-4 rounded-xl bg-blue-100">
             <h2 className="text-4xl font-bold text-center mb-4">Log Into Fintrack!</h2>
@@ -18,7 +30,7 @@ export default function LoginForm() {
                 <label className={labelStyle} htmlFor="password">Password</label>
                 <input className={inputStyle} required type="password" onChange={(e) => {setPassword(e.target.value)}} value={password} />
             </div>
-            <button className="w-60 bg-blue-600 text-white text-2xl p-2 rounded-lg mt-4">Login!</button>
+            <button className="w-60 bg-blue-600 text-white text-2xl p-2 rounded-lg mt-4" onClick={(e) => handleSubmit(e)}>Login!</button>
         </form>
     );
 }
