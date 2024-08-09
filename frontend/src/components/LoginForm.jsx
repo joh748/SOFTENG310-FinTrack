8c79de56ff54d90d60303368e42849690b456cab
Taks07 <yutakaoishi7@gmail.com>
2024-08-10 00:23:20 +1200

    Added use states to store input data

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,17 +1,22 @@
+import { useState } from "react";
+
 export default function LoginForm() {
     const labelStyle = "text-2xl";
-    const inputStyle = "border-2 border-blue-600 w-64 h-8 p-2 rounded-lg mb-4 mt-2";
+    const inputStyle = "border-2 w-64 h-8 p-2 rounded-lg mb-4 mt-2 border-black focus:border-blue-800 focus:outline-none";
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
     return (
         <form className="flex flex-col border-2 items-center border-blue-800 w-80 p-4 rounded-xl bg-blue-100">
             <h2 className="text-4xl font-bold text-center mb-4">Log Into Fintrack!</h2>
             <div className="flex flex-col">
                 <label className={labelStyle} htmlFor="email">Email</label>
-                <input className={inputStyle} type="email" id="email" name="email" />
+                <input className={inputStyle} required type="email" onChange={(e) => {setEmail(e.target.value)}} value={email} />
             </div>
             <div className="flex flex-col">
                 <label className={labelStyle} htmlFor="password">Password</label>
-                <input className={inputStyle} type="password" id="password" name="password" />
+                <input className={inputStyle} required type="password" onChange={(e) => {setPassword(e.target.value)}} value={password} />
             </div>
             <button className="w-60 bg-blue-600 text-white text-2xl p-2 rounded-lg mt-4">Login!</button>
         </form>
