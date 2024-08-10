02e8ef87529e880f58eb707e5bc1be1b0d8c8746
Taks07 <yutakaoishi7@gmail.com>
2024-08-10 17:09:17 +1200

    Set cors to allow patch requests

--- backend/src/config/corsConfig.js
+++ backend/src/config/corsConfig.js
@@ -10,7 +10,7 @@ const corsOptions = {
       callback(new Error('Not allowed by CORS'));
     }
   },
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 };
