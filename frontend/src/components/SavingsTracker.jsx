e7667f6944a3af1ce6a2c0bb8da6a5b810c1df6b
unknown <randyleean092m@gmail.com>
2024-08-13 11:41:30 +1200

    feat: added UI logic for updating progress bar
    feat: added tailwind styling on the elements
    
    refactor: couldn't use progress tag in styling with tailwind and rounded divs of 6px height didn't encapsulate the fill bar at 1%.

--- frontend/src/components/SavingsTracker.jsx
+++ frontend/src/components/SavingsTracker.jsx
@@ -4,19 +4,24 @@ import { useState, useEffect } from "react";
 export default function SavingsTracker() {
   const [balance, setBalance] = useState(0);
   const [goal, setGoal] = useState(0);
+  const progress = goal > 0 ? (balance / goal) * 100 : 0;
   return (
-    <div>
-      <h2> Current Savings Goal:</h2>
-      <p>
-        ${balance}/${goal}
-      </p>
-      <progress value={goal} max={goal}>
-        {goal}%
-      </progress>
-      <br />
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        Set New Goal
-      </button>
-    </div>
+    <>
+      <div className="flex flex-col items-center gap-2">
+        <h2 className="text-subheading"> Current Savings Goal</h2>
+        <p className="text-body">
+          ${balance}/${goal}
+        </p>
+        <div className="w-full h-6 bg-gray-200 dark:bg-gray-700 outline-primary-highlight outline outline-1">
+          <div
+            className="h-6 bg-primary-highlight"
+            style={{ width: `${progress}` }}
+          ></div>
+        </div>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-5 rounded">
+          Set New Goal
+        </button>
+      </div>
+    </>
   );
 }
