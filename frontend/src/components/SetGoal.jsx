af15a97cf58a06a3a715f62bf7dc53fb4500618c
unknown <randyleean092m@gmail.com>
2024-08-13 12:30:42 +1200

    feat: added basic layout of the soon-to-be pop-up modal

--- frontend/src/components/SetGoal.jsx
+++ frontend/src/components/SetGoal.jsx
@@ -16,7 +16,27 @@ export default function SetGoal({
 
   return (
     <>
-      <div>SetGoal</div>;
+      <div>
+        <div>
+          <h2>Set New Savings Goal</h2>
+          <input type="range" min="0" onChange={handleSliderChange}></input>
+          <input type="number" onChaneg={handleInputChange}></input>
+          <div>
+            <button
+              className=" bg-primary-highlight hover:bg-primary text-white font-bold py-2 px-4 rounded"
+              onClick={updateGoal}
+            >
+              Set New Goal
+            </button>
+            <button
+              className="bg-gray-500 text-white font-bold py-2 px-4 rounded mr-2"
+              onClick={closeModal}
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
