f01dc79889b663a3fd017e83b70ec529de3efeb5
unknown <randyleean092m@gmail.com>
2024-08-16 02:31:21 +1200

    fix: added balance state to also refresh the Current Balance value when user balance gets updated

--- frontend/src/components/CurrentBalance.jsx
+++ frontend/src/components/CurrentBalance.jsx
@@ -2,22 +2,23 @@ import { useEffect, useState, useContext } from "react";
 import TransactionContext from "../context/TransactionContext";
 
 export default function CurrentBalance() {
-    const { currency, convertCurrency, balance } = useContext(TransactionContext);
-    const [convertedAmount, setConvertedAmount] = useState(0);
+  const { currency, convertCurrency, balance } = useContext(TransactionContext);
+  const [convertedAmount, setConvertedAmount] = useState(0);
 
-    // useEffect check if each transaction is negative and then converts the currency
-    useEffect(() => {
-        const convert = async () => {
-            const convertedAmount = await convertCurrency(currency, 'NZD', balance); // using context to convert amount
-            setConvertedAmount(convertedAmount);
-        }
-        convert();
-    }, [currency]);
+  // useEffect check if each transaction is negative and then converts the currency
+  useEffect(() => {
+    const convert = async () => {
+      const convertedAmount = await convertCurrency(currency, "NZD", balance); // using context to convert amount
+      setConvertedAmount(convertedAmount);
+    };
+    convert();
+  }, [currency, balance]);
 
-    return (
-        <div className="flex flex-col items-center">
-            <h2 className="text-sub-heading font-extrabold">Current Balance: ${convertedAmount}</h2>
-        </div>
-    );
-
+  return (
+    <div className="flex flex-col items-center">
+      <h2 className="text-sub-heading font-extrabold">
+        Current Balance: ${convertedAmount}
+      </h2>
+    </div>
+  );
 }
