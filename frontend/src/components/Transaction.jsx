292d8a554fcff427f94c0d9b46c8a5c2d30853e7
unknown <randyleean092m@gmail.com>
2024-08-15 23:31:16 +1200

    feat: added a checkbox for each transaction to select for deletion. functionality not yet implemented.

--- frontend/src/components/Transaction.jsx
+++ frontend/src/components/Transaction.jsx
@@ -1,36 +1,41 @@
-import React, { useEffect, useState, useContext } from 'react';
-import TransactionContext from '../context/TransactionContext';
+import React, { useEffect, useState, useContext } from "react";
+import TransactionContext from "../context/TransactionContext";
 
-const Transaction = ({ transaction, }) => {
-    const [isAmountNegative, setIsAmountNegative] = useState();
-    const [convertedAmount, setConvertedAmount] = useState();
-    const { currency, convertCurrency } = useContext(TransactionContext);
+const Transaction = ({ transaction, handleSelect }) => {
+  const [isAmountNegative, setIsAmountNegative] = useState(false);
+  const [convertedAmount, setConvertedAmount] = useState(transaction.amount);
+  const { currency, convertCurrency } = useContext(TransactionContext);
 
-    // useEffect check if each transaction is negative and then converts the currency
-    useEffect(() => {
-        setIsAmountNegative(transaction.amount < 0);
-        const convert = async () => {
-            const converted = await convertCurrency(currency, 'NZD', transaction.amount); // using context to convert amount
-            setConvertedAmount(converted);
-        }
-        convert();
-    }, [currency]);
+  // useEffect to check if each transaction is negative and then convert the currency
+  useEffect(() => {
+    const convert = async () => {
+      const converted = await convertCurrency(
+        currency,
+        "NZD",
+        transaction.amount
+      ); // using context to convert amount
+      setConvertedAmount(converted);
+      setIsAmountNegative(converted < 0);
+    };
+    convert();
+  }, [currency, transaction.amount, convertCurrency]);
 
-    return (
-        <div>
-            {isAmountNegative ? (
-                <div className='flex flex-row justify-between text-body text-red-500 pl-[8px]'>
-                    <p className=' '>{convertedAmount}: {transaction.description}</p>
-                    <p className=''>{transaction.created_at.substring(0, 10)}</p>
-                </div>
-            ) : (
-                <div className='flex flex-row justify-between text-body text-green-500'>
-                    <p className=''>+{convertedAmount}: {transaction.description}</p>
-                    <p className=''>{transaction.created_at.substring(0, 10)}</p>
-                </div>
-            )}
-        </div>
-    );
+  return (
+    <div className="flex flex-row justify-start text-body pl-[8x]">
+      <input type="checkbox" onClick={() => handleSelect(transaction.id)} />
+      <div
+        className={` w-full flex flex-row justify-between text-body pl-[8px] ${
+          isAmountNegative ? "text-red-500" : "text-green-500"
+        }`}
+      >
+        <p>
+          {isAmountNegative ? convertedAmount : `+${convertedAmount}`}:{" "}
+          {transaction.description}
+        </p>
+        <p className="self-end">{transaction.created_at.substring(0, 10)}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Transaction;
