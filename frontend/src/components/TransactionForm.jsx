63039769d5206daae151d69151809725ac156d4f
unknown <randyleean092m@gmail.com>
2024-08-15 13:27:19 +1200

    feat: completed UI for adding new transaction, it's a pop-up modal again

--- frontend/src/components/TransactionForm.jsx
+++ frontend/src/components/TransactionForm.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
 export default function TransactionForm() {
+  const labelStyle = "text-2xl";
+  const inputStyle = "mt-2 border p-2 text-center w-full";
   return (
     <form>
       <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
@@ -11,6 +13,19 @@ export default function TransactionForm() {
           <h2 className="text-2xl font-bold mb-4 text-center">
             Add New Transaction
           </h2>
+          {/* amount,title,description */}
+          <div className="flex flex-col">
+            <label className={labelStyle}>Title</label>
+            <input className={inputStyle}></input>
+          </div>
+          <div className="flex flex-col">
+            <label className={labelStyle}>Amount</label>
+            <input className={inputStyle}></input>
+          </div>
+          <div className="flex flex-col">
+            <label className={labelStyle}>Description</label>
+            <input className={`${inputStyle} p-6`}></input>
+          </div>
           <div className="flex justify-center mt-4 gap-4">
             <button className=" bg-primary-highlight hover:bg-primary text-white font-bold py-2 px-4 rounded w-full">
               Apply
