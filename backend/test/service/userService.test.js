38518ef59d4fc766dc0d1cf7521c30561b06b7a7
joh748 <joh748@aucklanduni.ac.nz>
2024-09-22 23:08:41 +1200

    delete unused imporrt

--- backend/test/service/userService.test.js
+++ backend/test/service/userService.test.js
@@ -1,4 +1,4 @@
-const { expect, assert } = require('chai');
+const { expect } = require('chai');
 const sinon = require('sinon');
 const userService = require('../../src/services/userService');
 const pool = require('../../src/config/db');
