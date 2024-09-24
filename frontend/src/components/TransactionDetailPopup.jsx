e0fe663dcd42788e649d3aaf4d843695cec18216
tom1dev <tdev429@aucklanduni.ac.nz>
2024-09-24 12:26:29 +1200

    improved the formatting of the details popup

--- frontend/src/components/TransactionDetailPopup.jsx
+++ frontend/src/components/TransactionDetailPopup.jsx
@@ -5,21 +5,38 @@ export default function TransactionDetailPopup({transaction, setShowDetails}){
      setShowDetails(false);
    }
 
-  
   return(
       <>
         <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
           <div className="bg-white p-3 rounded-2xl w-96 relative flex flex-col">
-              <h2 className="text-sub-heading mx-auto pb-3">Transaction Details</h2>
+              
+              <h2 className="text-sub-heading font-bold mx-auto pb-3">Transaction Details</h2>
 
-              <h2 className="pl-2 text-body">Title: {transaction.title}</h2>
-        
-              <h2 className="pl-2 text-body">Amount: ${transaction.amount}</h2>
+              <div className= "flex flex-row">
+                <h2 className="pl-2 text-body font-bold">Title: </h2>
+                <h2 className="pl-2 text-body ">{transaction.title}</h2>
+              </div>
+              <div className= "flex flex-row">
+                <h2 className="pl-2 text-body font-bold">Amount:</h2>
+                <h2 className="pl-2 text-body">${transaction.amount}</h2>
+              </div>
+              
+              <h2 className="pl-2 pb-2 text-body font-bold">Description</h2>
+              
 
-              <h2 className="pl-2 text-body">Description: {transaction.description}</h2>
+              <textarea className="pl-4 font-size-12 font-medium resize-none"
+                value={transaction.description}
+                rows={6}
+              />
 
-              <button className="mr-0 ml-auto pt-3" onClick= {handleExit}>
-                 <div className="bg-primary-dark p-2 rounded-2xl w-20 relative"><h1 className="text-button-small text-white">exit</h1></div>
+              <button className="mr-0 ml-auto pt-5" onClick= {handleExit}>
+                <div id="exitdiv" 
+                 className="bg-primary-dark hover:bg-primary-darker p-2 rounded-2xl w-20 relative"
+                 >
+                    
+                    <h1 className="text-button-small text-white">exit</h1>
+                
+                </div>
               </button>
           </div>
         </div>
