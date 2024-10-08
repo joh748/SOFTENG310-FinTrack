390efefe478a7af70344e9371240c555238cfe42
joh748 <joh748@aucklanduni.ac.nz>
2024-10-08 17:29:39 +1300

    get rid of disableClick logic for the cancel button

--- frontend/src/components/transactions/TransactionForm.jsx
+++ frontend/src/components/transactions/TransactionForm.jsx
@@ -186,8 +186,7 @@ export default function TransactionForm({ onSubmit, onCancel }) {
             </button>
             <button
               onClick={onCancel}
-              className={`${disableClick ? disableStyle : enableCancelStyle} bg-gray-400 text-white font-bold py-2 px-4 rounded-full mr-2 w-full`}
-              disabled={disableClick}
+              className={`bg-gray-400 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded-full mr-2 w-full`}
             >
               Cancel
             </button>
